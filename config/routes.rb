8786f28ef9fa05262f5c769acb9140268ab5e389
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-08-29 19:22:40 -0500

    Downloaded devise and hooked up the views, controllers and User model. Added link to log in and flags in application.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/home", to: "home#index"
+  # get "/users/sign_out", to: "devise/sessions#destroy"
 end
